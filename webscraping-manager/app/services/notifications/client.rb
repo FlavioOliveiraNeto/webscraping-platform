437ea78ba652109d7b6d385d976585426fd0d8ca
FlavioOliveiraNeto <flavio.neto@goias.gov.br>
2026-02-09 17:14:12 -0300

    Adjusts data returned from notifications.

--- webscraping-manager/app/services/notifications/client.rb
+++ webscraping-manager/app/services/notifications/client.rb
@@ -1,3 +1,5 @@
+require 'faraday'
+
 module Notifications
   class Client
     def self.task_created(task)
@@ -10,6 +12,16 @@ module Notifications
       notify('task_created', task, data)
     end
 
+    def self.task_completed(task)
+      data = {
+        brand: task.brand,
+        model: task.model,
+        price: task.price,
+        completed_at: task.updated_at
+      }
+      notify('task_completed', task, data)
+    end
+
     def self.task_failed(task, error_msg)
       notify('task_failed', task, error: error_msg)
     end
@@ -17,19 +29,21 @@ module Notifications
     private
 
     def self.notify(event, task, extra = {})
-      url = ENV.fetch('NOTIFICATION_SERVICE_URL', 'http://localhost:3003')
+      url = ENV.fetch('NOTIFICATION_SERVICE_URL', 'http://notification-service:3000')
 
-      Faraday.post("#{url}/api/notifications") do |req|
-        req.headers['Content-Type'] = 'application/json'
-        req.body = {
-          event: event,
-          task_id: task.id,
-          user_id: task.user_id,
-          data: extra
-        }.to_json
+      begin
+        Faraday.post("#{url}/api/notifications") do |req|
+          req.headers['Content-Type'] = 'application/json'
+          req.body = {
+            event: event,
+            task_id: task.id,
+            user_id: task.user_id,
+            data: extra         
+          }.to_json
+        end
+      rescue Faraday::Error => e
+        Rails.logger.error("Failed to send notification: #{e.message}")
       end
-    rescue Faraday::Error => e
-      Rails.logger.error("Failed to send notification: #{e.message}")
     end
   end
 end
